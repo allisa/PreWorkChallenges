d4111cb0c9c5146392e7343c81dc08e583e8fc4d
Allisa LeBeuf <al5906@gmail.com>
2018-10-07 21:27:19 -0700

    challenge one solverd?

--- PreWorkChallenges/Program.cs
+++ PreWorkChallenges/Program.cs
@@ -6,7 +6,34 @@ namespace PreWorkChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write($"Your score is: {ChallengeOne()}.");
+        }
+
+        //Challenge one, Array Max Result
+        static int ChallengeOne()
+        {
+            int[] userInputArray = new int[5];
+
+            Console.WriteLine("Please enter 5 numbers between 1-10");
+
+            for (int i = 0; i < userInputArray.Length; i++)
+            {
+                userInputArray[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            Console.Write("Enter a number from your original input");
+            int numInput = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (int i = 0; i < userInputArray.Length; i++)
+            {
+                if (userInputArray[i] == numInput)
+                    count++;
+            }
+
+            return (numInput * count);
         }
     }
 }
